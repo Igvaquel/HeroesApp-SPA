a6703ccfd16cc4390ea99ba59e76f1cfc463af0c
ignacio vaquel <ignaciovaquel182@gmail.com>
2023-09-19 19:06:50 -0300

    Commit con la seccion 14 del curso

---
+++ src/heroes/routes/HeroesRoutes.jsx
@@ -0,0 +1,26 @@
+import { Navigate, Route, Routes } from "react-router-dom"
+import { Navbar } from "../components"
+import { DcPage, MarvelPage,HeroPage,SearchPage } from "../pages"
+
+
+export const HeroesRoutes = () => {
+  return (
+    <>
+        <Navbar/>
+
+        <div className="container">
+            <Routes>
+                <Route path="marvel" element={ <MarvelPage />}/>
+                <Route path="dc" element={ <DcPage />}/>
+
+                <Route path="hero/:id" element={ <HeroPage />}/>
+                <Route path="search" element={ <SearchPage />}/>
+
+
+
+                <Route path="/" element={ <Navigate to="/marvel" />}/>
+            </Routes>
+        </div>
+    </>
+  )
+}
